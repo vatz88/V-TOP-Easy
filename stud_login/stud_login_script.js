2a17943372e3376dc247d15da65758a57ed2dffd
Vatsal Joshi <vatz88@gmail.com>
2016-09-10 12:45:37 +0530

    stud_home.asp done.     card-panel added.     progress bar for attendence.

--- stud_login/stud_login_script.js
+++ stud_login/stud_login_script.js
@@ -1,3 +1,5 @@
-$("link[href='style.css']").remove(); //remove remove default css file
-$("body").addClass("container");
-$('body').load(chrome.extension.getURL("stud_login/stud_login.html")); //Content inside body tag will be replaced by content in stud_login.html
+$(function () {
+    $("link[href='style.css']").remove(); //remove remove default css file
+    $("body").addClass("container");
+    $('body').load(chrome.extension.getURL("stud_login/stud_login.html")); //Content inside body tag will be replaced by content in stud_login.html
+});
