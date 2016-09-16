0214c5bf296a8c5fa66bfb2f0408f753189f350c
Vatsal Joshi <vatz88@gmail.com>
2016-09-17 00:27:52 +0530

    wait for 5sec between each download trigger

--- coursepage_plan_display/coursepage_plan_display_script.js
+++ coursepage_plan_display/coursepage_plan_display_script.js
@@ -100,7 +100,16 @@ $(function () {
         while (i >= 0) {
             $("a").get(i).click();
             i--;
+            sleep(5000);
         }
     });
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds) {
+                break;
+            }
+        }
+    }
 
 });
