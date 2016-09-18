ebd54c6aacad579031dd65bba8f8722798a65d30
Vatsal Joshi <vatz88@gmail.com>
2016-09-19 00:31:24 +0530

    Message added to not completed pages

--- exam_schedule/exam_schedule_script.js
+++ exam_schedule/exam_schedule_script.js
@@ -1,5 +1,6 @@
 $(function () {
+    $('body').prepend('<p style="color:red">Materialized page comming soon</p>');
     //remove remove default css file
-    $('link[href="style.css"]').remove();
-    $('link[href="style2.css"]').remove();
+    // $('link[href="style.css"]').remove();
+    // $('link[href="style2.css"]').remove();
 });
